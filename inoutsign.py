868510e427084d880bca82f8b1caf9ac33ce7a96
Matt Derosier <derosier.matt@gmail.com>
2018-07-09 14:11:53 -0500

    Added a new line to the scroll.

--- inoutsign.py
+++ inoutsign.py
@@ -9,17 +9,17 @@ import RPi.GPIO as GPIO
 cnl = 18
 GPIO.setmode(GPIO.BOARD)
 
-docstat = 'In':
+docstat = 'In'
 
 class RunText(EditBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
 
     def Run(self):
-        if docstat = 'In':
+        if docstat == 'In':
             status = 'Doc is IN'
-            else:
-                status = 'Doc is OUT'
+        if docstat == 'Out':
+            status = 'Doc is OUT'
 
         offscreenCanvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
@@ -34,7 +34,7 @@ class RunText(EditBase):
 
         while True:
             offscreenCanvas.Clear()
-            len = graphics.DrawText(offscreenCanvas, font, pos, height, textColor, time.strftime ('%l:%M %p %b %d')) graphics.DrawText(offscreenCanvas, font, pos, height * 2, textColor, status)
+            len = graphics.DrawText(offscreenCanvas, font, pos, height, textColor, time.strftime ('%l:%M %p %b %d')); graphics.DrawText(offscreenCanvas, font, pos, height+10, textColor, status)
             pos -= 1
             if (pos + len < 0):
                 pos = offscreenCanvas.width
@@ -53,6 +53,6 @@ if __name__ == "__main__":
     parser = RunText()
     if (not parser.process()):
         parser.print_help()
-except KeyboardInterrupt:
+if KeyboardInterrupt:
     GPIO.cleanup()
     print('Exiting')
