f245866085337eb8b098112d5e74d3ea01c8e967
Matt Derosier <derosier.matt@gmail.com>
2018-07-12 10:40:55 -0500

    What have I done?

--- inoutsign2.py
+++ inoutsign2.py
@@ -26,6 +26,35 @@ class RunText(EditBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
 
+    def create_dir(file_path):
+        if not os.path.exists(file_path):
+            loggers.error("The file does not exist, %s", file_path)
+            raise IOError("The path is not a file")
+        os.makedirs(file_path)
+        loggers.info("Loading file from %s", file_path)
+        return file_path
+    def get_file(name_file):
+        #path = os.getcwd()
+        path = os.path.dirname(file)
+        file_path = os.path.join(path, name_file)
+        file_path1 = create_dir(file_path)
+        return file_path1
+    def load_font(font_text, font_size):
+        font_file = get_file('fonts/' + font_text)
+        if os.path.isfile(font_file):
+            if font_file.lower().endswith(".bdf"):
+                fonttwo = graphics.Font()
+                #font = font_file.format(font_size, font_size)
+                fonttwo.LoadFont(font_file)
+                fonttwo.CharacterWidth(font_size)
+            elif font_file.lower().endswith(".ttf") or font_file.lower().endswith(".otf"):
+                fonttwo = ImageFont.truetype(font_file, font_size)
+            elif font_file.lower().endswith(".pil"):
+                fonttwo = ImageFont.load(font_file)
+        return fonttwo
+            
+        
+
     def Run(self):
         
         status = 'On'
@@ -42,12 +71,12 @@ class RunText(EditBase):
         pos = offscreenCanvas.width
         #myText = time.strftime ('%l:%M%p')#self.args["text"]
         height = random.randint(10,22)
-        #emojistr = emoji.emojize(':tada: :sunrise: :bird: :cat: :alien: :volcano: :sunset: :tada:', use_aliases=True)
+        emojistr = emoji.emojize(':tada: :sunrise: :bird: :cat: :alien: :volcano: :sunset: :tada:', use_aliases=True)
 
         while True:
             statustext = status
             offscreenCanvas.Clear()
-            len = graphics.DrawText(offscreenCanvas, font, pos, height, textColor, time.strftime ('%l:%M %p %b %d')); graphics.DrawText(offscreenCanvas, font, pos+10, height+10, textColor, statustext); #graphics.DrawText(offscreenCanvas, fontTwo, pos, height+20, textColor, emojistr)
+            len = graphics.DrawText(offscreenCanvas, font, pos, height, textColor, time.strftime ('%l:%M %p %b %d')); graphics.DrawText(offscreenCanvas, font, pos+10, height+10, textColor, statustext); graphics.DrawText(offscreenCanvas, fonttwo, pos, height+20, textColor, emojistr)
             pos -= 1
             if (pos + len < 0):
                 pos = offscreenCanvas.width
